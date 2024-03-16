44b92b9612a7d9b31c734b51a1d7a4189a00ebf3
Rima Das <rimadas.automationtester@gmail.com>
2024-03-15 17:57:27 -0700

    Adding Find Car Test testcase

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import base.BasePage;
+
+public class HomePage extends BasePage{
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver,this);
+	}
+
+	@FindBy(xpath="//*[@id=\"root\"]/div[1]/header/div/nav/ul/li[1]/div")
+	public WebElement newCars;
+	
+	@FindBy(linkText="Find New Cars")
+	public WebElement findNewCars;	
+	
+	public void findNewCar() throws InterruptedException {
+		
+		new Actions(driver).moveToElement(newCars).perform();
+		Thread.sleep(2000);
+		findNewCars.click();
+		Thread.sleep(2000);	
+
+	}
+
+}
