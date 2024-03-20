a173a4f56b65203136dd4abe1d5885547c3593db
Rima Das <rimadas.automationtester@gmail.com>
2024-03-19 17:23:32 -0700

    Adding code to create a file and add the car models and price in the file in CarBase class

--- src/main/java/base/CarBase.java
+++ src/main/java/base/CarBase.java
@@ -1,7 +1,12 @@
 package base;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -14,8 +19,7 @@ public class CarBase {
 	public CarBase(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
-	}
-	
+	}	
 	
 
 	// Code to get the Title of Cars from New Cars Page
@@ -28,8 +32,7 @@ public class CarBase {
 		return carTitle.getText();
 
 	}
-	
-	
+		
 
 	// Code to get Model name and Price of a particular Car Brand
 
@@ -39,12 +42,17 @@ public class CarBase {
 
 	@FindBy(xpath = "//*[@id=\"root\"]/div[2]/div/div[3]/div[1]/ul/li/div/div/div/div[3]/div/span/span[1]")
 	public List<WebElement> carPrice;
+	
+	//Adding the array list so that we can print the output in a file
+	public ArrayList<String> list = new ArrayList<String>();
 
 	public void getCarModelName() {
 
 		for (int i = 0; i < carModel.size(); i++) {
 
 			String modelNames = carModel.get(i).getText();
+			//At each index the data will be added to list
+			list.add(modelNames);
 			System.out.println(modelNames);
 		}
 
@@ -55,9 +63,40 @@ public class CarBase {
 		for (int i = 0; i < carPrice.size(); i++) {
 
 			String modelPrice = carPrice.get(i).getText();
+			list.add(modelPrice);
 			System.out.println(modelPrice);
 		}
 
+	}
+	
+	public void addCarModelInFile(String fileName) {
+		
+		String info = fileName + " car brand name and price\n";
+		File file = new File(fileName);
+		
+		try {
+			FileWriter fw = new FileWriter(file, true);
+			
+			fw.write(info);
+			String lineSeparator = System.getProperty("line.separator");
+			
+			for(int i = 0;i < list.size(); i++) {
+				fw.write(list.get(i));
+				fw.write(lineSeparator);
+			}
+			
+			fw.flush();
+			fw.close();
+			
+			FileUtils.copyFile(file, new File("/Users/rimadas/eclipse-workspace/SeleniumPageObjectModelFramework/target/CarModelAndPrice/" + fileName + ".txt"));
+			FileUtils.deleteQuietly(file);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
